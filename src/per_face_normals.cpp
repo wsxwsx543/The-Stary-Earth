15037b8e6dcebc64cdadc4245ed1431d5302b275
wsxwsx543 <wusixuan543543@gmail.com>
2020-06-19 14:45:11 -0400

    calculate per face normals(triangle mesh)

--- src/per_face_normals.cpp
+++ src/per_face_normals.cpp
@@ -6,8 +6,14 @@ void per_face_normals(
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & N)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code:
   N = Eigen::MatrixXd::Zero(F.rows(),3);
-  ////////////////////////////////////////////////////////////////////////////
+  int rows = F.rows();
+
+  for (int r = 0; r < rows; r++) {
+    Eigen::RowVector3d a(V.row(F(r, 0)));
+    Eigen::RowVector3d b(V.row(F(r, 1)));
+    Eigen::RowVector3d c(V.row(F(r, 2)));
+    
+    N.row(r) = triangle_area_normal(a, b, c);
+  }
 }
