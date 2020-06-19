e922047c2cc18520ce99c9acf860e0741bf9e129
wsxwsx543 <wusixuan543543@gmail.com>
2020-06-18 20:13:16 -0400

    finish write to object file

--- src/write_obj.cpp
+++ src/write_obj.cpp
@@ -13,8 +13,52 @@ bool write_obj(
   const Eigen::MatrixXi & NF)
 {
   assert((F.size() == 0 || F.cols() == 3 || F.cols() == 4) && "F must have 3 or 4 columns");
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
-  return false;
+
+  // Reference: https://en.wikipedia.org/wiki/Wavefront_.obj_file
+  std::ofstream(file);
+  if (!file) {
+    std::cout << "Cannot open the file: " << filename << "." << std::endl;
+    return false;
+  }
+  
+  // the order based on wikipedia.
+  // wirte V with identifier v
+  for (int i = 0; i < V.rows(); i++) {
+    file << "v";
+    for (int j  = 0; j < V.cols(); j++) {
+      file << " " << V(i, j);
+    }
+    file << std::endl;
+  }
+
+  // write texture coordinates (UV with identifier vt)
+  for (int i = 0; i < UV.rows(); i++) {
+    file << "vt";
+    for (int j = 0; j < UV.rows(); j++) {
+      file << " " << UV(i, j);
+    }
+    file << std::endl;
+  }
+
+  // write vertex normals (NV with identifier vn)
+  // Note: vn should not have to be normal.
+  for (int i = 0; i < NV.rows(); i++) {
+    file << "vn";
+    for (int j = 0; j < NV.cols(); j++) {
+      file << " " << NV(i, j);
+    }
+    file << std::endl;
+  }
+
+  assert(F.size() == UF.size() && F.size() == NF.size() && "size of F, UF and NF should be same.");
+  // write faces with identifier f
+  // f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3 ...
+  for (int i = 0; i < F.rows(); i++) {
+    file << "f";
+    for (int j = 0; j < F.cols(); j++) {
+      file << " " << F(i, j) << "/" << UF(i, j) << "/" << NF(i, j);
+    }
+    file << std::endl;
+  }
+  return true;
 }
